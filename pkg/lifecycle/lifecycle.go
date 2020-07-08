8dfe3047ce6f0d891147eba2ebfd0c2c3d16cad4
mlboy <ml3@meitu.com>
2020-07-08 20:17:55 +0800

    wip

---
+++ pkg/lifecycle/lifecycle.go
@@ -0,0 +1,62 @@
+package lifecycle
+
+import (
+	"sync"
+	"sync/atomic"
+
+	"golang.org/x/sync/errgroup"
+)
+
+//Cycle ..
+type Cycle struct {
+	mu uint32
+	sync.Once
+	eg   *errgroup.Group
+	quit chan struct{}
+}
+
+//NewCycle new a cycle life
+func NewCycle() *Cycle {
+	return &Cycle{
+		mu:   0,
+		eg:   &errgroup.Group{},
+		quit: make(chan struct{}),
+	}
+}
+
+//Run a new goroutine
+func (c *Cycle) Run(fn func() error) {
+	c.eg.Go(fn)
+}
+
+//Done block and return a chan error
+func (c *Cycle) Done() <-chan error {
+	errCh := make(chan error)
+	go func() {
+		if err := app.eg.Wait(); err != nil {
+			errCh <- err
+		}
+		close(errCh)
+	}()
+	return errCh
+}
+
+//DoneAndClose ..
+func (c *Cycle) DoneAndClose() {
+	<-c.Done
+	c.Close()
+}
+
+//Close ..
+func (c *Cycle) Close() {
+	if c.mu == 0 {
+		if atomic.CompareAndSwapUint32(&c.mu, 0, 1) {
+			close(c.quit)
+		}
+	}
+}
+
+// Wait blocked for a life cycle
+func (c *Cycle) Wait() <-chan struct{} {
+	return <-c.quit
+}

9e2793861a35d08db41f0614df90d026af05b94f
mlboy <mlboy@126.com>
2021-11-17 23:13:29 +0800

    feat: 更新

--- example/gin/main.go
+++ example/gin/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"log"
-	"runtime"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rushteam/beauty"
@@ -14,22 +13,18 @@ var confFile = flag.String("config", "./config.yaml", "config path")
 
 func main() {
 	flag.Parse()
-	// var c config.Config
-	// conf.LoadConfig(*confFile, &c)
 	app := beauty.New(
-	// beauty.WithServer(web.New(
-	// 	"api",
-	// 	web.WithAddr(":8080"),
-	// 	web.WithRouter(router),
-	// )),
-	)
-	err := app.Run(
-		web.New(
-			"api",
-			web.WithAddr(":8080"),
+		web.WithServer(
 			router,
+			web.WithAddr(":8081"),
 		),
+		// beauty.WithServer(web.MustNew(
+		// 	"api",
+		// 	web.WithAddr(":8080"),
+		// 	web.WithRouter(router),
+		// )),
 	)
+	err := app.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,8 +33,5 @@ func main() {
 func router(r *web.WebServer) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "hi beauty")
-	})
-	r.GET("/version", func(c *gin.Context) {
-		c.String(200, runtime.Version())
 	})
 }

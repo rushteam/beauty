789a268039aa80dd73812ed8c4748a12f77c0317
马良 <mlboy@126.com>
2023-08-15 16:00:06 +0800

    fix

--- example/example/main.go
+++ example/example/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/rushteam/beauty"
 )
@@ -11,7 +12,11 @@ import (
 func main() {
 	s := &srv{}
 	s2 := &srv{}
-	app := beauty.New(beauty.WithService(s, s2), beauty.WithWebServer(":8080"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+	app := beauty.New(beauty.WithService(s, s2), beauty.WithWebServer(":8080", mux))
 	if err := app.Start(context.Background()); err != nil {
 		log.Fatalln(err)
 	}

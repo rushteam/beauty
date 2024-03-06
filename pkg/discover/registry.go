4ce149904d2e0449194d5a585296844a10413bb0
马良 <mlboy@126.com>
2024-03-06 09:18:44 +0800

    迭代

--- pkg/discover/registry.go
+++ pkg/discover/registry.go
@@ -1,10 +1,35 @@
 package discover
 
+import "net"
+
 type Endpoint interface {
 	ServiceName() string
+	Addr() net.Addr
+	Weight() int
+	Metadata() map[string]string
 }
 
 type Registry interface {
 	Register(info Endpoint) error
 	Deregister(info Endpoint) error
+}
+
+type EndpointBasicInfo struct {
+	serviceName string
+	addr        net.Addr
+	weight      int
+	metadata    map[string]string
+}
+
+func (s EndpointBasicInfo) ServiceName() string {
+	return s.serviceName
+}
+func (s EndpointBasicInfo) Addr() net.Addr {
+	return s.addr
+}
+func (s EndpointBasicInfo) Weight() int {
+	return s.weight
+}
+func (s EndpointBasicInfo) Metadata() map[string]string {
+	return s.metadata
 }

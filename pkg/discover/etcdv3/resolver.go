c6d1fb9749a223ed609b3eadfcff0e419f33663c
马良 <mlboy@126.com>
2024-03-18 03:50:23 +0800

    迭代

--- pkg/discover/etcdv3/resolver.go
+++ pkg/discover/etcdv3/resolver.go
@@ -1,7 +1,6 @@
 package etcdv3
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -51,10 +50,8 @@ func (r *etcdResolver) watch() {
 		if err != nil {
 			continue
 		}
-		addrs := getAddrs(endpoints)
-		fmt.Println(addrs)
 		r.cc.UpdateState(resolver.State{
-			Addresses: addrs,
+			Addresses: getAddrs(endpoints),
 		})
 	}
 }
@@ -68,53 +65,12 @@ func (r *etcdResolver) Close() {
 	r.watcher.ctx.Done()
 }
 
-// func (r *etcdResolver) watch(ctx context.Context) {
-// if r.stopped() {
-// 	return
-// }
-// var (
-// 	endpoints  = make(map[string]*discover.ServiceInfo)
-// 	rev        int64
-// 	retryTimes int
-// )
-
-// for {
-// 	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
-// 	resp, err := r.client.Get(ctx, r.key, clientv3.WithPrefix())
-// 	cancel()
-// 	fmt.Println("watch >", r.key, resp, err)
-// 	if err != nil {
-// 		log.Printf("[error]failed to resolve addr, caused by %s", err)
-// 		delay := r.backoff(retryTimes)
-// 		retryTimes++
-// 		time.Sleep(delay)
-// 		continue
-// 	}
-
-// 	retryTimes = 0
-// 	rev = resp.Header.Revision
-
-// 	for _, kv := range resp.Kvs {
-// 		v := discover.ServiceInfo{}
-// 		if err := v.Unmarshal(kv.Value); err == nil {
-// 			endpoints[string(kv.Key)] = &v
-// 		}
-// 	}
-// 	addrs := r.getAddrs(endpoints)
-// 	r.cc.UpdateState(resolver.State{
-// 		Addresses: addrs,
-// 	})
-// 	break
-// }
-// }
-
 func getAddrs(endpoints map[string]*discover.ServiceInfo) []resolver.Address {
 	addrs := make([]resolver.Address, 0, len(endpoints))
 	for _, v := range endpoints {
 		if v == nil {
 			continue
 		}
-		// addr.Metadata = &v.Metadata // the addr.Metadata will be hashed, so we should use pointer
 		addrs = append(addrs, resolver.Address{
 			Addr:       v.Addr,
 			ServerName: v.Name,
